bb9bfc0eec48f004fb8330660d42d54c6022503e
Ted Barnett <tedbarnett@users.noreply.github.com>
2023-05-05 12:55:36 -0400

    Create RENAME_config.py

---
+++ RENAME_config.py
@@ -0,0 +1,9 @@
+# REPLACE the string "tedsmith" below with any text string that always appears in your "from" field
+# This ensures that only emails from this user are included in the output
+#
+username = 'tedsmith'
+
+# REPLACE the string below with your OpenAI API Key
+# NOTE: Not in use in v1.0
+#
+openai_api_key = "PASTE IN YOUR OPENAI API KEY HERE"
